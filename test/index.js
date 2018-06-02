e91729e0449f16e641d63bcc0545afffae685edb
Luke Edwards <luke.edwards05@gmail.com>
2018-06-02 15:02:29 -0700

    update tests after spec fix

--- test/index.js
+++ test/index.js
@@ -15,15 +15,23 @@ test('exports', t => {
 });
 
 test('commonjs :: module.exports', t => {
-	let fn = foo('module.exports = function (a, b) { return a + b; }');
-	t.is(typeof fn, 'function', 'returns a function from contents');
-	t.is(fn(18, 24), 42, '~> function is valid');
+	let mod = foo('module.exports = function (a, b) { return a + b; }');
+	t.is(typeof mod, 'object', 'returns a object from contents');
+	t.ok(!!mod.default, '~> contains "default" key');
+
+	t.is(typeof mod.default, 'function', '~~> is function');
+	t.is(mod.default(18, 24), 42, '~> function is valid');
 	t.end();
 });
 
 test('commonjs :: exports.*', t => {
-	let obj = foo('exports.foo = 123; exports.bar = function (val) { return `${val}-${exports.foo}`; };');
-	t.is(typeof obj, 'object', 'returns an object from contents');
+	let mod = foo('exports.foo = 123; exports.bar = function (val) { return `${val}-${exports.foo}`; };');
+	t.is(typeof mod, 'object', 'returns an object from contents');
+
+	t.ok(!!mod.default, '~> contains "default" key');
+	t.is(typeof mod.default, 'object', '~~> is object');
+
+	let obj = mod.default;
 	t.is(Object.keys(obj).length, 2, '~> object has two keys');
 	t.is(obj.foo, 123, 'received valid number type from `exports.foo`');
 	t.is(typeof obj.bar, 'function', 'received valid function type from `exports.bar`');
@@ -32,17 +40,26 @@ test('commonjs :: exports.*', t => {
 });
 
 test('commonjs :: module.exports.*', t => {
-	let obj = foo('module.exports.foo = 123; module.exports.bar = function (val) { return `${val}-${module.exports.foo}`; };');
-	t.is(typeof obj, 'object', 'returns an object from contents');
+	let mod = foo('module.exports.foo = 123; module.exports.bar = function (val) { return `${val}-${module.exports.foo}`; };');
+	t.is(typeof mod, 'object', 'returns an object from contents');
+
+	t.ok(!!mod.default, '~> contains "default" key');
+	t.is(typeof mod.default, 'object', '~~> is object');
+
+	let obj = mod.default;
 	t.is(Object.keys(obj).length, 2, '~> object has two keys');
-	t.is(obj.foo, 123, 'received valid number type from `exports.foo`');
-	t.is(typeof obj.bar, 'function', 'received valid function type from `exports.bar`');
+	t.is(obj.foo, 123, 'received valid number type from `module.exports.foo`');
+	t.is(typeof obj.bar, 'function', 'received valid function type from `module.exports.bar`');
 	t.is(obj.bar('hello'), 'hello-123', '~> function is valid');
 	t.end();
 });
 
 test('umd', t => {
-	let fn = foo('!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):e.import=t()}(this,function(){return function(a, b){ return a + b }});');
+	let mod = foo('!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):e.import=t()}(this,function(){return function(a, b){ return a + b }});');
+	t.is(typeof mod, 'object', 'returns an object from contents');
+	t.ok(!!mod.default, '~> contains "default" key');
+
+	let fn = mod.default;
 	t.is(typeof fn, 'function', 'returns a function from contents');
 	t.is(fn(18, 24), 42, '~> function is valid');
 	t.end();
