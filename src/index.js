de3a414ecf49776c27fdb59d5ed5c93dfd4d5c82
Luke Edwards <luke.edwards05@gmail.com>
2019-04-05 00:23:33 -0700

    feat: import “module” build

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
+function reset(key, tag) {
+	tag.onerror = tag.onload = null;
+	URL.revokeObjectURL(tag.src);
+	delete window[key];
+	tag.remove();
+}
+
 export default function (url) {
-	return fetch(url).then(r => r.text()).then(txt => {
-		let m = { exports:{} };
-		new Function('module', 'exports', txt)(m, m.exports);
-		m.default = m.exports.default || m.exports; // boo
-		return m;
-	});
+	try {
+		return new Function(`return import('${url}')`).call();
+	} catch (tag) {
+		return new Promise((res, rej, key) => {
+			tag = document.createElement('script');
+			key = '$' + Math.random().toString(32).slice(2);
+			url = new URL(url, location.href).href;
+			tag.type = 'module';
+			tag.onerror = () => {
+				reset(key, tag);
+				rej(new TypeError(`Failed to fetch dynamically imported module: ${url}`));
+			};
+			tag.onload = () => {
+				res(window[key]);
+				reset(key, tag);
+			};
+			tag.src = URL.createObjectURL(
+				new Blob([`import * as m from '${url}';window.${key}=m;`], {
+					type: 'application/javascript'
+				})
+			);
+			document.head.appendChild(tag);
+		});
+	}
 }
