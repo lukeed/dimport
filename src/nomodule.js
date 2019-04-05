662c2bc1b04712c5732c83136190e272d084d5f5
Luke Edwards <luke.edwards05@gmail.com>
2019-04-05 00:24:42 -0700

    feat: import “nomodule” build

---
+++ src/nomodule.js
@@ -0,0 +1,61 @@
+import imports from 'rewrite-imports';
+
+var CACHE = {};
+
+function rewrite(str, url, fn) {
+	var key, keys=[], urls=[], idx=0;
+
+	var txt = imports(
+		str
+			// Ensure full URLs & Gather static imports
+			.replace(/(import\s*.*\s*from\s*)['"]([^'"]+)['"];?/gi, (_, state, loc) => {
+				loc = "'" + new URL(loc, url).href + "'";
+				return state + (/\s*from\s*/.test(state) ? `'$dimport[${urls.push(loc) - 1}]';` : `${loc};`);
+			})
+
+			// Attach ourself for dynamic imports
+			// .replace(/(^|\s|;)(import)\((.*)\)/g, `$1${fn}($3)`)  //<- also works
+			.replace(/(^|\s|;)(import)(?=\()/g, '$1' + fn)
+
+			// Exports
+			.replace(/export default/, 'module.exports =')
+			.replace(/export\s+(const|function|class|let|var)\s+(.+?)(?=(\(|\s|\=))/gi, (_, type, name) => keys.push(name) && (type + ' ' + name))
+			.replace(/export\s*\{([\s\S]*?)\}/gi, (_, list) => {
+				var tmp, out='', arr=list.split(',');
+				while (tmp = arr.shift()) {
+					tmp = tmp.trim().split(' as ');
+					out += `exports.${tmp[1] || tmp[0]} = ${tmp[0]};\n`;
+				}
+				return out;
+			})
+	, 'eval');
+
+	for (keys.sort(); key = keys.shift();) {
+		txt += '\nexports.' + key + ' = ' + key + ';';
+	}
+
+	return urls.length ? `return Promise.all([${urls.join()}].map(${fn})).then(function($dimport){${txt}});` : txt;
+}
+
+function dimport(url) {
+	url = new URL(url, location.href).href;
+	if (CACHE[url]) return Promise.resolve(CACHE[url]);
+	return fetch(url).then(r => r.text()).then((txt, mod) => {
+		mod = { exports:{} };
+		window.dimport = dimport;
+		return Promise.resolve(
+			new Function('module', 'exports', rewrite(txt, url, 'window.dimport'))(mod, mod.exports)
+		).then(() => {
+			mod.exports.default = mod.exports.default || mod.exports;
+			return CACHE[url] = mod.exports;
+		});
+	});
+}
+
+export default function (url) {
+	try {
+		return new Function(`return import('${url}')`).call();
+	} catch (err) {
+		return dimport(url);
+	}
+}
